b3f32dba9d4c637934467fca78e79c1cd65e5737
B3ttm3nsch <krueger.rico@web.de>
2018-06-13 15:07:41 +0200

    Renamed Labels

--- app/app/models/bsag_mep_audio.rb
+++ app/app/models/bsag_mep_audio.rb
@@ -10,20 +10,19 @@ class BsagMepAudio < ActiveRecord::Base
 
   validates :version_number, uniqueness: { scope: [:project_partial, :audio_type, :audio_format, :mep_version] }
   validates :bsag_audio_fault_id, presence: true
-  validates :dbtp_max_before_type_cast, :lra_before_type_cast, :lufs_before_type_cast,
-            numericality: { only_decimal: true },
-            allow_blank: true,
-            format: { with: /\A[+-]?\d{0,2}(?!\d)(.\d{0,1})?\z/ }
   validates_length_of :version_name_ext, maximum: 30
   validates_length_of :is_complete_comment,
                       :is_in_sync_comment,
                       :channel_error_comment,
                       :audio_fault_comment,
                       maximum: 255
-
+  # validates decimal format
+  validates :dbtp_max_before_type_cast, :lra_before_type_cast, :lufs_before_type_cast,
+            numericality: { only_decimal: true },
+            allow_blank: true,
+            format: { with: /\A[+-]?\d{0,2}(?!\d)(.\d{0,1})?\z/ }
   validate :receiving_date_cannot_be_in_the_future if validates_presence_of :receiving_date
   def receiving_date_cannot_be_in_the_future
     errors.add(:receiving_date, "can't be in the future") if receiving_date.presence && receiving_date > Date.today
   end
-
 end
